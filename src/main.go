d4bd5af01577638b85db6575fbbd6b5c15a910bc
Andrei Iarysh <andrei.iarysh@tv4.se>
2022-07-09 16:44:58 +0200

    Part 1. Introduction and Setting up the REPL

--- src/main.go
+++ src/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func printPrompt() {
+	fmt.Print("db > ")
+}
+
+func readInput(inputScanner *bufio.Scanner) {
+	inputScanner.Scan()
+	bytesRead := len(inputScanner.Bytes())
+
+	if bytesRead <= 0 {
+		fmt.Println("Error reading input")
+		os.Exit(1)
+	}
+}
+
+func main() {
+	inputScanner := bufio.NewScanner(os.Stdin)
+	for {
+		printPrompt()
+		readInput(inputScanner)
+
+		if inputScanner.Text() == ".exit" {
+			os.Exit(0)
+		} else {
+			fmt.Printf("Unrecognized command %q.\n", inputScanner.Bytes())
+		}
+	}
+}
