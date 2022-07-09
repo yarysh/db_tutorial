9ee3e6ac51d73a15f0ec38d27b9c6d47e2464f28
Andrei Iarysh <andrei.iarysh@tv4.se>
2022-07-09 17:42:56 +0200

    Part 2. World's Simplest SQL Compiler and VM

--- src/main.go
+++ src/main.go
@@ -2,9 +2,35 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 )
+
+type MetaCommandResult int
+
+const (
+	MetaCommandSuccess = MetaCommandResult(iota)
+	MetaCommandUnrecognizedCommand
+)
+
+type PrepareResult int
+
+const (
+	PrepareSuccess = PrepareResult(iota)
+	PrepareUnrecognizedStatement
+)
+
+type StatementType int
+
+const (
+	StatementInsert = StatementType(iota)
+	StatementSelect
+)
+
+type Statement struct {
+	Type StatementType
+}
 
 func printPrompt() {
 	fmt.Print("db > ")
@@ -20,16 +46,63 @@ func readInput(inputScanner *bufio.Scanner) {
 	}
 }
 
+func doMetaCommand(inputScanner *bufio.Scanner) MetaCommandResult {
+	if bytes.Equal(inputScanner.Bytes(), []byte(".exit")) {
+		os.Exit(0)
+	}
+	return MetaCommandUnrecognizedCommand
+}
+
+func prepareStatement(inputScanner *bufio.Scanner, statement *Statement) PrepareResult {
+	if bytes.Equal(inputScanner.Bytes()[0:6], []byte("insert")) {
+		statement.Type = StatementInsert
+		return PrepareSuccess
+	}
+	if bytes.Equal(inputScanner.Bytes(), []byte("select")) {
+		statement.Type = StatementSelect
+		return PrepareSuccess
+	}
+
+	return PrepareUnrecognizedStatement
+}
+
+func executeStatement(statement Statement) {
+	switch statement.Type {
+	case StatementInsert:
+		fmt.Println("This is where we would do an insert.")
+		break
+	case StatementSelect:
+		fmt.Println("This is where we would do a select.")
+		break
+	}
+}
+
 func main() {
 	inputScanner := bufio.NewScanner(os.Stdin)
 	for {
 		printPrompt()
 		readInput(inputScanner)
 
-		if inputScanner.Text() == ".exit" {
-			os.Exit(0)
-		} else {
-			fmt.Printf("Unrecognized command %q.\n", inputScanner.Bytes())
+		if inputScanner.Bytes()[0] == '.' {
+			switch doMetaCommand(inputScanner) {
+			case MetaCommandSuccess:
+				continue
+			case MetaCommandUnrecognizedCommand:
+				fmt.Printf("Unrecognized command %q\n", inputScanner.Bytes())
+				continue
+			}
 		}
+
+		statement := Statement{}
+		switch prepareStatement(inputScanner, &statement) {
+		case PrepareSuccess:
+			break
+		case PrepareUnrecognizedStatement:
+			fmt.Printf("Unrecognized keyword at start of %q.\n", inputScanner.Bytes())
+			continue
+		}
+
+		executeStatement(statement)
+		fmt.Println("Executed.")
 	}
 }
